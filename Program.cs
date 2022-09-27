699ea8b94d1fc2a8412ab388a04cd543d1b89364
MatheusOliveira1996 <matheus.hortolandia@gmail.com>
2022-09-26 23:09:06 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Heranca.Entities;
+
+namespace Heranca
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.00, 500.0);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
